e492874e146f8f60ec861ad43778d3bb058f6871
Madelyn Romero <m.romero013@gmail.com>
2026-03-10 17:15:01 -0600

    Add name, age specific validation specs

--- spec/models/dino_spec.rb
+++ spec/models/dino_spec.rb
@@ -8,5 +8,36 @@ RSpec.describe Dino, type: :model do
                 expect(build(:dino)).to be_valid
             end
         end
+
+        context "validates presence of" do
+            it { is_expected.to validate_presence_of(:name) }
+            it { is_expected.to validate_presence_of(:age) }
+        end
+
+        context "age specific validations" do
+            it { is_expected.to validate_numericality_of(:age).only_integer }
+            it { is_expected.to validate_numericality_of(:age).is_greater_than_or_equal_to(0) }
+            it { is_expected.to validate_numericality_of(:age).is_less_than_or_equal_to(100) }
+
+
+            it 'is invalid without age' do
+                dino = build(:dino, age: nil)
+                expect(dino).not_to be_valid
+                expect(dino.errors[:age]).to include("can't be blank")
+            end
+
+            it 'is invalid with age over AGE_PARAM' do
+                dino = build(:dino, age: 101)
+                expect(dino).not_to be_valid
+            end
+
+            it 'is valid at age boundary of 0' do
+                expect(build(:dino, age: 0)).to be_valid
+            end
+
+            it 'is valid at age boundary of 100' do
+                expect(build(:dino, age: 100)).to be_valid
+            end
+        end
     end
 end
