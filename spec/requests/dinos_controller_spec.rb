48aed3889417c990b6c329ed579c0625a20d771c
Madelyn Romero <m.romero013@gmail.com>
2026-03-10 17:38:03 -0600

    Require rails_helper, set up basic index test

--- spec/requests/dinos_controller_spec.rb
+++ spec/requests/dinos_controller_spec.rb
@@ -1,4 +1,22 @@
+require "rails_helper"
+
 RSpec.describe 'Dinos', type: :request do
     let(:valid_dino)   { create(:dino) }
     let(:invalid_dino) { build(:dino, age: nil) }
+
+    describe 'GET /dinos' do
+        it 'returns http success' do
+            get dinos_path
+            expect(response).to have_http_status(:success)
+        end
+
+        it 'displays all dino names' do
+            create(:dino, name: 'Rex')
+            create(:dino, name: 'Trike')
+            get dinos_path
+
+            expect(response.body).to include('Rex')
+            expect(response.body).to include('Trike')
+        end
+    end
 end
