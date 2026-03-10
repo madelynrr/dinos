485aab8b5fea981cf601b7444b2a08c8c7073936
Madelyn Romero <m.romero013@gmail.com>
2026-03-10 14:59:43 -0600

    Set up enums, validations on dino model

--- app/models/dino.rb
+++ app/models/dino.rb
@@ -1,4 +1,8 @@
 class Dino < ApplicationRecord
     validates :name, :health, :comment, :age_metrics, presence: true
     validates :age, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0}
+
+    enum :category, {herbivore: 0, carnivore: 1}, validate: true
+    enum :period, {Cretaceous: 0, Jurassic: 1}, validate: true
+    enum :diet, {plants: 0, meat: 1}, validate: true
 end
