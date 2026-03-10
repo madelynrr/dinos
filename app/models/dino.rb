83e875714e0139f7bf5f930a29a587069ef15555
Madelyn Romero <m.romero013@gmail.com>
2026-03-10 16:20:32 -0600

    Use cleaner enum definition on dino model

--- app/models/dino.rb
+++ app/models/dino.rb
@@ -6,9 +6,9 @@ class Dino < ApplicationRecord
     validates :name, :health, :comment, :age_metrics, presence: true
     validates :age, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0}
 
-    enum :category, {herbivore: 0, carnivore: 1}, validate: true
-    enum :period, {Cretaceous: 0, Jurassic: 1}, validate: true
-    enum :diet, {plants: 0, meat: 1}, validate: true
+    enum :category, [:herbivore, :carnivore], validate: true
+    enum :period, [:Cretaceous, :Jurassic], validate: true
+    enum :diet, [:plants, :meat], validate: true
 
     # Though only used once, this type of number feels safer as a constant to me. If scaling, easier to update.
     AGE_PARAM = 100
